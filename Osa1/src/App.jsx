2adb7dc7fc53d2f047707ae9a85686302c12e09f
Yasminmikaela <yasmin.haanpaa@gmail.com>
2025-08-15 13:57:37 +0300

    1.7: tehtävän palautus

--- Osa1/src/App.jsx
+++ Osa1/src/App.jsx
@@ -6,13 +6,18 @@ import { useState } from 'react'
   </button>
 )*/
 
+
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-
+  // const [allClicks, setAll] = useState([])
+  // const [total, setTotal] = useState(0)
+  const total = good + neutral + bad
+  const average = total === 0 ? 0 : (good - bad) / total
+  const percentage = good / total * 100
 
   return (
     <div>
@@ -27,7 +32,12 @@ const App = () => {
         Bad
       </button>
       <h2>Statistics</h2>
-        Good: {good} Neutral: {neutral} Bad: {bad}
+        Good: {good} <br />
+        Neutral: {neutral} <br />
+        Bad: {bad} <br />
+        Total: {total} <br />
+        Average: {average} <br />
+        Positive: {percentage.toFixed(1) + "%"}
     </div>
   )
 }
